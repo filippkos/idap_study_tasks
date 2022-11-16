8083ea70a2cbcd9cff52b6bb4c9d9b7203b448ea
Filipp Kosenko <filippkosgmc@gmail.com>
2022-11-16 15:22:32 +0200

    Created basic project.

---
+++ idap_study_task8/idap_study_task8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idap_study_task8
+//
+//  Created by Dima Omelchenko on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
