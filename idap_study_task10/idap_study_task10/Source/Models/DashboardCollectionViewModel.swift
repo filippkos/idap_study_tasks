5e57f17c97eff489bd73287331e052f3edff6b05
Filipp Kosenko <filippkosgmc@gmail.com>
2023-03-10 10:45:02 +0200

    Added SwiftGen. Created PagerView class.

---
+++ idap_study_task10/idap_study_task10/Source/Models/DashboardCollectionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardCollectionViewModel.swift
+//  idap_study_task10
+//
+//  Created by Filipp Kosenko on 08.03.2023.
+//
+
+import Foundation
+
+struct DashboardCollectionViewModel {
+    
+    
+}
