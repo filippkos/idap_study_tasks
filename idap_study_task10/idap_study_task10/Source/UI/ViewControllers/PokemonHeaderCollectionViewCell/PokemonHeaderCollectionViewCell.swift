2eb8efccdf78614d476ebc8d30f3208f8f394725
Filipp Kosenko <filippkosgmc@gmail.com>
2023-05-07 00:38:55 +0300

    Added VerticalTagView to PokemonListView cells and PokemonViewHeaderCell. Added String capitalizing first letter extension. Changed cells layout.

--- idap_study_task10/idap_study_task10/Source/UI/ViewControllers/PokemonHeaderCollectionViewCell/PokemonHeaderCollectionViewCell.swift
+++ idap_study_task10/idap_study_task10/Source/UI/ViewControllers/PokemonHeaderCollectionViewCell/PokemonHeaderCollectionViewCell.swift
@@ -22,22 +22,42 @@ class PokemonHeaderCollectionViewCell: UICollectionViewCell {
     @IBOutlet var weightValue: UILabel!
     @IBOutlet var orderTitle: UILabel!
     @IBOutlet var orderValue: UILabel!
+    @IBOutlet var verticalTagView: VerticalTagViewController!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.flowLayoutConfigure()
     }
     
     // MARK: -
     // MARK: Public
     
     func configure(with model: Pokemon, indexPath: IndexPath) {
-        self.nameLabel.text = model.grouped[1]?.1.first
+        self.nameLabel.text = model.grouped[1]?.1.first?.capitalizingFirstLetter()
         self.heightTitle.text = model.grouped[3]?.0
         self.heightValue.text = "\(model.grouped[3]?.1.first ?? "") cm"
         self.weightTitle.text = model.grouped[4]?.0
         self.weightValue.text = "\(model.grouped[4]?.1.first ?? "") kg"
         self.orderTitle.text = model.grouped[6]?.0
         self.orderValue.text = model.grouped[6]?.1.first
+    }
+    
+    func configure(with model: PokemonCollectionViewCellModel) {
+        self.verticalTagView?.configure(with: model.items)
+    }
+    
+    func flowLayoutConfigure() {
+        let itemWidth = 100
+        let itemHeight = 35
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.estimatedItemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 2
+        self.verticalTagView?.collectionView.collectionViewLayout = layout
+        self.verticalTagView?.collectionView.isPagingEnabled = false
+        self.verticalTagView?.collectionView.alwaysBounceVertical = false
+        self.verticalTagView?.collectionView.isScrollEnabled = false
     }
 }
