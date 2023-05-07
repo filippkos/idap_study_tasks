8d6154da3cfa5879243de8b1e0684d14ba635691
Filipp Kosenko <filippkosgmc@gmail.com>
2023-05-07 16:53:45 +0300

    Added pokemon types images to assets. Added images in chip tags.

--- idap_study_task10/idap_study_task10/Source/UI/Views/VerticalTagView/VerticalTagViewController.swift
+++ idap_study_task10/idap_study_task10/Source/UI/Views/VerticalTagView/VerticalTagViewController.swift
@@ -24,6 +24,7 @@ class VerticalTagViewController: NibDesignable, UICollectionViewDelegate, UIColl
     // MARK: Variables
 
     var items: [VerticalTagItem] = []
+    var isImagesEnabled: Bool = false
     
     // MARK: -
     // MARK: Overrided
@@ -42,6 +43,49 @@ class VerticalTagViewController: NibDesignable, UICollectionViewDelegate, UIColl
         self.items = models
         self.collectionView.reloadData()
     }
+    
+    func getImage(typeName: String) -> UIImage {
+        switch typeName {
+        case "fighting":
+            return Images.PokemonTypeIcons.fighting.image
+        case "psychic":
+            return Images.PokemonTypeIcons.psychic.image
+        case "poison":
+            return Images.PokemonTypeIcons.poison.image
+        case "dragon":
+            return Images.PokemonTypeIcons.dragon.image
+        case "ghost":
+            return Images.PokemonTypeIcons.ghost.image
+        case "dark":
+            return Images.PokemonTypeIcons.dark.image
+        case "ground":
+            return Images.PokemonTypeIcons.ground.image
+        case "fire":
+            return Images.PokemonTypeIcons.fire.image
+        case "fairy":
+            return Images.PokemonTypeIcons.fairy.image
+        case "water":
+            return Images.PokemonTypeIcons.water.image
+        case "flying":
+            return Images.PokemonTypeIcons.flying.image
+        case "normal":
+            return Images.PokemonTypeIcons.normal.image
+        case "rock":
+            return Images.PokemonTypeIcons.rock.image
+        case "electric":
+            return Images.PokemonTypeIcons.electric.image
+        case "bug":
+            return Images.PokemonTypeIcons.bug.image
+        case "grass":
+            return Images.PokemonTypeIcons.grass.image
+        case "ice":
+            return Images.PokemonTypeIcons.icy.image
+        case "steel":
+            return Images.PokemonTypeIcons.steel.image
+        default:
+            return UIImage()
+        }
+    }
 
     // MARK: -
     // MARK: Data Source
@@ -52,8 +96,11 @@ class VerticalTagViewController: NibDesignable, UICollectionViewDelegate, UIColl
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(cellClass: ChipCollectionViewCell.self, indexPath: indexPath)
-        cell.image?.isHidden = true
         cell.text?.text = self.items[indexPath.row].title
+        if self.isImagesEnabled {
+            cell.image.image = getImage(typeName: self.items[indexPath.row].title)
+            
+        }
         cell.backgroundColor = Colors.Colors.wildSand.color
         
         return cell
