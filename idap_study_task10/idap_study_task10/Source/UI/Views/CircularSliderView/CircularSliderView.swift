aa70fcd4778868ae01c3eb8ec1a60479c01f1044
Filipp Kosenko <filipp.kosenko@idapgroup.com>
2023-05-19 22:33:22 +0300

    Added gradient on circular slider thumb.

--- idap_study_task10/idap_study_task10/Source/UI/Views/CircularSliderView/CircularSliderView.swift
+++ idap_study_task10/idap_study_task10/Source/UI/Views/CircularSliderView/CircularSliderView.swift
@@ -17,9 +17,10 @@ class CircularSliderView: NibDesignable {
     
     // MARK: -
     // MARK: Variables
-    
-    var thumbLabel = UILabel()
+ 
     var labelValue = 0
+    var thumbLabel = UILabel()
+    var labelBackground = UIView()
     
     // MARK: -
     // MARK: Public
@@ -39,7 +40,7 @@ class CircularSliderView: NibDesignable {
                 Attributes.hasThumb(true),
                 Attributes.thumbRadius(15.5),
                 Attributes.thumbShadowRadius(0),
-                Attributes.thumbShadowDepth(0)
+                Attributes.thumbShadowDepth(0),
             ]
         )
     }
@@ -48,12 +49,38 @@ class CircularSliderView: NibDesignable {
     // MARK: Private
     
     private func configureLabel() {
+        let center = self.slider?.thumbCenter ?? CGPoint()
+        self.prepareLabelBackground(with: center)
+        self.prepareThumbLabel(with: center)
+    }
+    
+    private func prepareLabelBackground(with point: CGPoint) {
+        self.labelBackground.frame = CGRect(origin: point, size: CGSize(width: 30, height: 30))
+        self.labelBackground.layer.cornerRadius = 15
+        self.labelBackground.backgroundColor = .white
+        self.labelBackground.center = point
+        self.insertSubview(self.labelBackground, at: 3)
+        self.labelBackground.layer.insertSublayer(self.configureGradient(), at: 0)
+    }
+    
+    private func prepareThumbLabel(with point: CGPoint) {
         self.thumbLabel.text = self.labelValue.description
         self.thumbLabel.font = Fonts.PlusJakartaSans.medium.font(size: 15)
-        self.thumbLabel.textColor = .black
-        self.thumbLabel.center = self.slider?.thumbCenter ?? CGPoint()
+        self.thumbLabel.textColor = .white
+        self.thumbLabel.center = point
         self.addSubview(self.thumbLabel)
         self.thumbLabel.sizeToFit()
+    }
+    
+    private func configureGradient() -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.cornerRadius = 10
+        gradientLayer.frame = self.labelBackground.bounds
+        gradientLayer.locations = [0.0, 0.9]
+        gradientLayer.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradientLayer.cornerRadius = 15
+
+        return gradientLayer
     }
     
     // MARK: -
@@ -69,7 +96,7 @@ class CircularSliderView: NibDesignable {
         
         let center: CGPoint = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
         var trackRadius = (self.slider?.outerControlRadius ?? 0) - 4
-        let circlePath = UIBezierPath(arcCenter: center, radius: trackRadius ?? CGFloat(), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: center, radius: trackRadius, startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
             
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circlePath.cgPath
